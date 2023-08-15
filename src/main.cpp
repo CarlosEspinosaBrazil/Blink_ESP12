c85de203931218333e8722dfbc21625959ca7d86
CarlosEspinosaBrazil <c_e_espinosa@hotmail.com>
2023-08-15 10:33:02 -0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+size_t count = 3;
+
+void setup() {
+    pinMode(LED_BUILTIN,OUTPUT);
+    digitalWrite(LED_BUILTIN,HIGH);
+}
+
+void loop() {
+    for (size_t i = 0; i < count; i++){
+        digitalWrite(LED_BUILTIN,LOW);
+        delay(30);
+        digitalWrite(LED_BUILTIN,HIGH);
+        delay(50);
+    }
+    delay(500);
+}
+
